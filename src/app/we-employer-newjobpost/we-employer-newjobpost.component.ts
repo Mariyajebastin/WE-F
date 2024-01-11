066ff81893b289bcf4540d6702f8bc9d3a88c028
jebastin <jebastin@gmail.com>
2024-01-11 15:29:40 +0530

    corrections

--- src/app/we-employer-newjobpost/we-employer-newjobpost.component.ts
+++ src/app/we-employer-newjobpost/we-employer-newjobpost.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-we-employer-newjobpost',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./we-employer-newjobpost.component.css']
 })
 export class WeEmployerNewjobpostComponent {
+  public jobForm : FormGroup | any;
 
+  constructor() {
+    this.jobForm = new FormGroup({
+      job_title : new FormControl(),
+      job_location : new FormControl(),
+      min_salary : new FormControl(),
+      max_salary : new FormControl(),
+      job_description : new FormControl(),
+      company_profile : new FormControl()
+    })
+  }
+
+  doPost(){
+    const formData = this.jobForm.value;
+
+    console.log("Form Data",formData);
+  }
 }
